0b55c3859ab19704519b6a89f68b419c36b1c1ad
unknown <a@.(none)>
2013-10-12 11:12:10 +0800

    trying for commonjs

--- defaultfilter.js
+++ defaultfilter.js
@@ -1,14 +1,23 @@
 "use strict";
 
-window.anod.Topia.DefaultFilter = Backbone.Model.extend({
+//window.anod.Topia.DefaultFilter = Backbone.Model.extend({
+(function(){
+
 
 	defaults: {
 		singleStrengthMinOccur: 3,
 		noLimitStrength: 2
 	},
 	
-    call: function(word, occur, strength) {
-        return ((strength == 1 && occur >= this.get('singleStrengthMinOccur')) ||
-                (strength >= this.get('noLimitStrength')));
-    }
-});
+	call: function(word, occur, strength) {
+			return ((strength == 1 && occur >= this.get('singleStrengthMinOccur')) ||
+							(strength >= this.get('noLimitStrength')));
+	}
+//});
+
+
+if (typeof module !== 'undefined' && typeof module.exports !== 'undefined')
+    module.exports = DefaultFilter;
+  else
+    window.DefaultFilter = DefaultFilter;		
+})();
